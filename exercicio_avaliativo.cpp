7c0d70af3fcf7a2993fabea050486fb2eb9f619f
SamuelJOliveira <s4mueljacome@outlook.com>
2025-06-20 11:02:51 -0300

    atualizacao bezout

--- exercicio_avaliativo.cpp
+++ exercicio_avaliativo.cpp
@@ -2,44 +2,6 @@
 #include <string>
 using namespace std;
 
-// todas as suas funções aqui...
-/*
-int piso(float n){
-    int k;
-    if (n >= 0){
-    k = 0;
-    bool achou_piso = false;
-
-        while (!achou_piso){
-
-            if (k <= n && k+1 > n){
-                achou_piso = true;
-            } else {
-                k++;
-            }
-        }
-        if (achou_piso){
-            return k;
-        }
-    }
-    else {
-        k = -1;
-        bool achou_piso = false;
-        while (!achou_piso){
-
-            if (k <= n && k+1 > n){
-                achou_piso = true;
-            } else {
-                k--;
-            }
-        }
-        if (achou_piso){
-            return k;
-        }
-    }
-    return k;
-*/
-
     int piso(float n) {
     int k = (n >= 0) ? 0 : -1;
 
@@ -48,37 +10,21 @@ int piso(float n){
     }
 
     return k;
-}
-
-
-int teto(float n){
-    int k = (n >= 0) ? 0 : -1;
-
-    while (!(k <= n && k-1 < n)){
-        k += (n >= 0) ? 1 : -1;
-    }    
-    
-    return k;
-}
-
-//int div(int a, int b;) {
- /*
- int q, r;
-    q = ((a > 0 && b < 0) || (a < 0 && b > 0)) ? -1 : 0;
-
-    r = -1;
-
-    int modulo_b;
-
-    modulo_b = (b < 0) ? -b : b;
-
-    while (!( (a == b * q + r) && (0 <= r && r < modulo_b) )){
-        q += ((a > 0 && b < 0) || (a < 0 && b > 0)) ? -1 : 1;
-        r = a - (b * q);
     }
 
-    return q;
- */   
+
+
+    int teto(float n){
+        int k = (n >= 0) ? 0 : -1;
+
+        while (!(k <= n && k-1 < n)){
+            k += (n >= 0) ? 1 : -1;
+        }    
+        
+        return k;
+    }
+
+
 
     int meu_div(int a, int b) {
 
@@ -116,7 +62,7 @@ int teto(float n){
         return a - b * q;
     }
 
-    int primos_entre(int a, int b) {
+    void primos_entre(int a, int b) {
         
         int qntd_primos = 0;
         
@@ -141,18 +87,126 @@ int teto(float n){
                 qntd_primos++;
             }
         }
-    // Imprimir os primos encontrados
-    cout << "Primos entre " << a << " e " << b << ": ";
-    for (int i = 0; i < qntd_primos; i++) {
-        cout << primos[i] << " ";
+        // Imprimir os primos encontrados
+        cout << "Primos entre " << a << " e " << b << ": ";
+        for (int i = 0; i < qntd_primos; i++) {
+            cout << primos[i] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-}
     
     string cesar(string entrada){
+
         string codificada = "";
         for (int i = 0; i < entrada.length(); i++) {
-            codificada = ((entrada[i] + '3' - entrada[i]) + 1) % 26;
-        } 
+            if (entrada[i] >= 'A' && entrada[i] <= 'Z'){
+                codificada += 'A' + ((entrada[i] + 3 - 'A') % 26);
+            }
+            else if(entrada[i] >= 'a' && entrada[i] <= 'z'){
+                    codificada += 'a' + ((entrada[i] + 3 - 'a') % 26);
+                }
+            else {
+                codificada += entrada[i];
+            }
+        }
         return codificada;
     } 
+
+    int mdc(int a, int b) {
+
+        cout << "Caculando mdc(" << a << "," << b << ")" << endl;
+
+        if (b == 0) {
+            cout << "O mdc encontrado foi " << a << endl;
+            return a;
+
+        }
+        return mdc(b, a % b);
+    }
+
+    int bezout(int a, int b, int& x, int& y){
+
+        cout << "Caculando mdc(" << a << "," << b << ")" << endl;
+
+        if (b == 0){
+            cout << "O mdc encontrado foi " << a << endl;
+            x = 1;
+            y = 0;
+            cout << "    [x=" << x << ", y=" << y << "]\n\n";
+            return a;
+        }
+
+        int x1, y1;
+
+        int mdc = bezout(b, a % b, x1, y1);
+
+        x = y1;
+        y = x1 - (a / b) * y1;
+                                    //b x1 + (a%b) y1 = mdc
+                                    //a x + b y = mdc
+                                    // b x1 + (a - a/b * b) y1 = mdc
+                                    // b x1 + a * y1 - a/b * b * y1 = mdc
+                                    // a * y1 + b (x1 * -a/b * y1) = a x + b y 
+                                    // x = y1 --- y = (x1 * -a/b * y1)
+
+        cout << "Para (a=" << a << ", b=" << b << "):\n";
+        cout << "x = y1 = " << y1 << "\n";
+        cout << "y = x1 - (a/b)*y1 = " << x1 << " - (" << a << "/" << b << ")*" << y1 << " = " << y << "\n\n";
+
+        return mdc;
+    }
+
+
+    
+
+    // main piso
+    int main() {
+    float fn;
+    cin >> fn;
+    cout << piso(fn) << "\n";
+    }
+
+    // main teto
+    int main() {
+    float fn;
+    cin >> fn;
+    cout << teto(fn) << "\n";
+    }
+
+    //div
+    int main() {
+        int a, b;
+        cin >> a >> b;
+        cout << meu_div(a, b) << "\n";       
+    }   
+
+    //mod
+    int main() {
+         int a, b;
+         cin >> a >> b;
+         cout << meu_mod(a, b) << "\n";
+    }
+
+
+
+
+
+
+
+
+    // main bezout
+    int main() {
+        int a, b;
+        cout << "Digite a e b: ";
+        cin >> a >> b;
+
+        int x, y;
+        int m = bezout(a, b, x, y);
+
+        cout << "Resultado Final\n";
+        cout << "MDC(" << a << ", " << b << ") = " << m << "\n";
+        cout << "Coeficientes de Bezout: s = " << x << ", t = " << y << "\n";
+        cout << a << "*" << x << " + " << b << "*" << y << " = " << m << "\n";
+        return 0;
+    }
+    
